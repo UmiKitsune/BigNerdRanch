4ffbb14d1ffb16f55d02cc73bfd6beb1c55e0467
Чернигова Екатерина Владимировна <rinachernigova@gmail.com>
2023-01-23 14:38:50 +0300

    3rd lesson + exercises

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.geoquiz
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity.TOP
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.properties.Delegates
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var questionTextView: TextView
+    private lateinit var percentTV: TextView
     private lateinit var nextButton: ImageButton
+    private lateinit var questionLayout: LinearLayout
+    private lateinit var rightAnswersLayout: LinearLayout
 
     private val questionBank = listOf(
         Question(R.string.q_australia, true),
@@ -24,16 +28,24 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.q_americas, true),
         Question(R.string.q_asia, true),
     )
+
+    private var rightAnswers = 0.0
     private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate called")
         setContentView(R.layout.activity_main)
 
         questionTextView = findViewById(R.id.question_text_view)
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        questionLayout = findViewById(R.id.question_layout)
+        rightAnswersLayout = findViewById(R.id.right_answers_layout)
+        percentTV = findViewById(R.id.percent)
+
+        updateQuestion()
 
         trueButton.setOnClickListener { view: View ->
             checkAnswer(true)
@@ -44,29 +56,70 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener { view: View ->
-            currentIndex = (currentIndex + 1) % questionBank.size
+            //currentIndex = (currentIndex + 1) % questionBank.size
+            currentIndex += 1
             updateQuestion()
         }
 
         questionTextView.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+            currentIndex += 1
             updateQuestion()
         }
 
-        updateQuestion()
+    }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
     }
 
     private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
-        questionTextView.setText(questionTextResId)
+        if (currentIndex < questionBank.size) {
+            val questionTextResId = questionBank[currentIndex].textResId
+            questionTextView.setText(questionTextResId)
+            trueButton.isEnabled = true
+            falseButton.isEnabled = true
+        } else {
+            questionLayout.visibility = View.GONE
+            rightAnswersLayout.visibility = View.VISIBLE
+            val percent = ((rightAnswers / questionBank.size) * 100).toInt()
+            percentTV.text = resources.getString(R.string.percent_text, percent, "%")
+        }
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = questionBank[currentIndex].answer
         val messageResId =
-            if (userAnswer == correctAnswer)  R.string.correct_toast else R.string.incorrect_toast
+            if (userAnswer == correctAnswer) {
+                rightAnswers += 1
+                R.string.correct_toast
+            } else {
+                rightAnswers += 0
+                R.string.incorrect_toast
+            }
         toastTop(messageResId)
+        trueButton.isEnabled = false
+        falseButton.isEnabled = false
     }
 
     private fun toastTop(textRes: Int) {
